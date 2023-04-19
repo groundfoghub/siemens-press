2b973bba7ab0c40a6cc376e3f72c95ebf0bf5e59
Christina Koenig <christina.koenig-ext@querplex.biz>
2023-04-19 17:54:16 +0200

    Add columns adjustments

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -13,6 +13,29 @@ export default function decorate(block) {
           picWrapper.classList.add('columns-img-col');
         }
       }
+
+      // follow class naming pattern kebab style for franklin boilerplate components
+
+      const headingIsPTag = col.children[0]?.tagName === 'P';
+
+      if (headingIsPTag && !pic) {
+        col.children[0].classList.add('columns-heading');
+      } else if (col.firstElementChild?.classList.contains('headline')) {
+        const headlineElement = col.querySelector('h1')
+        || col.querySelector('h2')
+        || col.querySelector('h3');
+
+        headlineElement.classList.add('columns-heading');
+      } else if (col.firstElementChild?.tagName === 'H1'
+        || col.firstElementChild?.tagName === 'H2'
+        || col.firstElementChild?.tagName === 'H3'
+      ) {
+        col.firstElementChild.classList.add('columns-heading');
+      }
+      const colChildren = [...col.children];
+      colChildren.shift();
+
+      colChildren.forEach((child) => child.classList.add('columns-description'));
     });
   });
 }
