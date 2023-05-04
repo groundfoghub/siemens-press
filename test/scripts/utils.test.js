7d1d7fc207a1ce708d5ceb453f6711c5d825d6e9
Antonio Bianchetti <antonio.bianchetti@groundfog.cloud>
2023-05-04 23:19:59 +0200

    Extended unit tests

--- test/scripts/utils.test.js
+++ test/scripts/utils.test.js
@@ -1,20 +1,23 @@
-/* global describe */
+/* global describe it */
 import { expect } from '@esm-bundle/chai';
 import { isStringDate } from '../../scripts/utils.js';
 
-[
-  ['Date with long month', '2 September 2023', true],
-  ['Date with short month', '2 Sep 2023', true],
-  ['All numerid date', '2 11 2023', true],
-  ['All numerid date', '02-11-2023', true],
-  ['All words date', 'First, September, 2000', true],
-  ['All numerid date', '', false],
-  ['All numerid date', undefined, false],
-  ['All numerid date', 'Adobe Franklin', false],
-].forEach((spec) => {
-  describe(`isStringDate() -> ${spec[0]}`, () => {
-    const actual = isStringDate(spec[1]);
-
-    expect(actual).to.equal(spec[2]);
+describe('isStringDate', () => {
+  [
+    ['returns true when is a date with long month', '2 September 2023', true],
+    ['returns true when is a date with short month', '2 Sep 2023', true],
+    ['returns true when is an all numerid date', '2 11 2023', true],
+    ['returns true when is an all numerid date', '02-11-2023', true],
+    ['returns true when is an all numerid date', '2022-05-04', true],
+    ['returns true when is a date and time', '2022-05-04T15:30:00', true],
+    ['returns true when is a date and time and timezone ', '2022-05-04T15:30:00-07:00', true],
+    ['returns true when date is in verbose', 'First, September, 2000', true],
+    ['returns false when given an empty string', '', false],
+    ['returns false when given undefined', undefined, false],
+    ['returns false when given an invalid date string', 'Adobe Franklin', false],
+  ].forEach((spec) => {
+    it(spec[0], () => {
+      expect(isStringDate(spec[1])).to.equal(spec[2]);
+    });
   });
 });
