c0f25bc2e2b41390cf04c8034d1b908533950545
Antonio Bianchetti <antonio.bianchetti@groundfog.cloud>
2023-05-18 18:30:24 +0200

    Second level working in Burger Menu

--- blocks/burgermenu/burgermenu.js
+++ blocks/burgermenu/burgermenu.js
@@ -1,4 +1,75 @@
-const createNavigation = (navigationBlock, classNames) => {
+const closeInnerLevel = (block) => {
+  const wrapper = block.querySelector('.newHomeBurgerMenu__navigationWrapper');
+
+  wrapper.classList.remove('newHomeBurgerMenu__secondNavLevel');
+  wrapper.classList.add('newHomeBurgerMenu__firstNavLevel');
+
+  block.querySelector('.newHomeBurgerMenu__currentPosition').remove();
+  block.querySelector('.newHomeBurgerMenu__showOverviewWrapper').remove();
+  block.querySelector('.newHomeBurgerMenu__navigationRight').remove();
+};
+
+const openInnerLevel = (block, element) => {
+  const wrapper = block.querySelector('.newHomeBurgerMenu__navigationWrapper');
+
+  wrapper.classList.remove('newHomeBurgerMenu__firstNavLevel');
+  wrapper.classList.add('newHomeBurgerMenu__secondNavLevel');
+
+  // Create the back button
+  const backButton = document.createElement('button');
+  backButton.classList.add('newHomeBurgerMenu__currentPosition');
+  backButton.setAttribute('aria-label', 'Burger Menu back button');
+  backButton.setAttribute('data-ste-element', 'Main menu');
+  backButton.addEventListener('click', () => closeInnerLevel(block));
+
+  // Create the arrow icon for the back button
+  const arrowIcon = document.createElement('span');
+  arrowIcon.classList.add('newHomeBurgerMenu__goBack', 'newHomeBurgerMenu__arrowBack');
+
+  // Create the current title span
+  const currentTitle = document.createElement('span');
+  currentTitle.classList.add('newHomeBurgerMenu__currentTitle');
+  currentTitle.setAttribute('tabindex', '-1');
+  currentTitle.textContent = 'Main menu';
+
+  // Append the arrow icon and current title to the back button
+  backButton.appendChild(arrowIcon);
+  backButton.appendChild(currentTitle);
+
+  // Create the show overview wrapper div
+  const showOverviewWrapper = document.createElement('div');
+  showOverviewWrapper.classList.add('newHomeBurgerMenu__showOverviewWrapper');
+
+  // Create the show overview link
+  const showOverviewLink = element.querySelector('a');
+  showOverviewLink.classList.add('newHomeBurgerMenu__showOverview', 'newHomeBurgerMenu__showOverviewAppear');
+
+  showOverviewWrapper.appendChild(showOverviewLink);
+
+  // Create the inner navigation list
+  const firstLevel = block.querySelector('.newHomeBurgerMenu__firstLevel');
+  firstLevel.append(backButton, showOverviewWrapper);
+
+  const navigationBody = block.querySelector('.newHomeBurgerMenu__navigationBody');
+
+  const navigationRight = document.createElement('div');
+  navigationRight.classList.add('newHomeBurgerMenu__navigationRight', 'newHomeBurgerMenu__navigatedForward');
+  const other = document.createElement('div');
+  other.classList.add('newHomeBurgerMenu__otherLevel');
+
+  const innerNavigation = element.querySelector('ul');
+
+  [...innerNavigation.children].forEach((li) => {
+    li.classList.add('newHomeBurgerMenu__item');
+    [...li.children][0]?.classList.add('newHomeBurgerMenu__naviItem');
+  });
+
+  other.append(innerNavigation);
+  navigationRight.append(other);
+  navigationBody.append(navigationRight);
+};
+
+const createNavigation = (navigationBlock, classNames, block) => {
   const navigation = document.createElement('ul');
   navigation.classList.add(...classNames.ul);
   [...navigationBlock.children].forEach((element) => {
@@ -10,11 +81,13 @@ const createNavigation = (navigationBlock, classNames) => {
     primaryNavItem.classList.add(...classNames.li);
     navLink.classList.add(...classNames.a);
     navLink.textContent = navLinkData.textContent;
-    navLink.href = navLinkData.href;
     primaryNavItem.append(navLink);
 
     if (subNavigation) {
       navLink.classList.add('newHomeBurgerMenu__naviItem--noLink');
+      navLink.addEventListener('click', () => openInnerLevel(block, element.cloneNode(true)));
+    } else {
+      navLink.href = navLinkData.href;
     }
 
     navigation.append(primaryNavItem);
@@ -65,7 +138,7 @@ export default function decorate(block) {
         a: ['newHomeLink', 'newHomeBurgerMenu__link'],
       };
     }
-    const navigation = createNavigation(ul, classNames);
+    const navigation = createNavigation(ul, classNames, block);
 
     div5.appendChild(navigation);
   });
